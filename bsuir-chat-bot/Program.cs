330afea5caafed6d7aecd93dac3f246267a4d59f
Dmitry Nevedomsky <dimchik01050@gmail.com>
2018-03-10 02:13:09 +0300

    vk api

--- bsuir-chat-bot/Program.cs
+++ bsuir-chat-bot/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Exception;
+using VkNet.Model.RequestParams;
 
 namespace bsuir_chat_bot
 {
@@ -20,6 +24,36 @@ namespace bsuir_chat_bot
             Console.WriteLine($"{configuration["accesstoken"]}");
             Console.WriteLine($"{configuration["shortenerapikey"]}");
             Console.WriteLine("Press a key...");
+            Console.ReadKey();
+            
+            var api = new VkApi();
+	
+            api.Authorize(new ApiAuthParams
+            {
+                ApplicationId = ulong.Parse(configuration["appid"]),
+                Login = configuration["login"],
+                Password = configuration["password"],
+                Settings = Settings.All
+            });
+            
+            Console.WriteLine(api.Token);
+
+            int n = 0;
+            try
+            {
+                while (!Console.KeyAvailable)
+                {
+                    var r = api.Groups.Get(new GroupsGetParams());
+                    n++;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine($"N = {n}");
+            }
+            
             Console.ReadKey();
         }
     }
