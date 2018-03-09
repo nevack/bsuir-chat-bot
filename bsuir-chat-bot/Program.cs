52498045206734fb5cb3947c37c0895e9a4fbcce
Dmitry Nevedomsky <dimchik01050@gmail.com>
2018-03-10 01:36:06 +0300

    Initial commit

---
+++ bsuir-chat-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bsuir_chat_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
