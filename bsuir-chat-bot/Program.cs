41f81a2636207dac99beaeed4d21388c473cf068
Dmitry Nevedomsky <dimchik01050@gmail.com>
2018-03-10 01:53:06 +0300

    added packages, config

--- bsuir-chat-bot/Program.cs
+++ bsuir-chat-bot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace bsuir_chat_bot
 {
@@ -6,7 +8,19 @@ namespace bsuir_chat_bot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("botconfig.json");
+
+            var configuration = builder.Build();
+
+            Console.WriteLine($"{configuration["appid"]}");
+            Console.WriteLine($"{configuration["login"]}");
+            Console.WriteLine($"{configuration["password"]}");
+            Console.WriteLine($"{configuration["accesstoken"]}");
+            Console.WriteLine($"{configuration["shortenerapikey"]}");
+            Console.WriteLine("Press a key...");
+            Console.ReadKey();
         }
     }
 }
