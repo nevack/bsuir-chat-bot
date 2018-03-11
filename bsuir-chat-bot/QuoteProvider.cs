4693735662c68c2b574dc24d485a0a38fb34646c
Dmitry Nevedomsky <dimchik01050@gmail.com>
2018-03-11 18:16:10 +0300

    Random quotes

--- bsuir-chat-bot/QuoteProvider.cs
+++ bsuir-chat-bot/QuoteProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace bsuir_chat_bot
@@ -11,14 +12,16 @@ namespace bsuir_chat_bot
         
         private QuoteDictionary _quotedict;
         private string _quoteFile;
+        private Random Random;
 
         internal QuoteProvider(string quoteFile)
         {
             ReloadQuotes(quoteFile);
 
+            Random = new Random();
             Functions = new Dictionary<string, Func<List<string>, string>>
             {
-                {"quote", list => GetQuote(Convert.ToInt32(list[0])).Text}
+                {"quote", GetQuoteString}
             };
         }
 
@@ -29,12 +32,15 @@ namespace bsuir_chat_bot
             return _quotedict.Quotes[index];
         }
         
-        private string GetQuoteString(int index)
+        private string GetQuoteString(List<string> args)
         {
+            var index = !args.Any() ? Random.Next(0, _quotedict.Quotes.Count) : int.Parse(args[0]);
+            
             if (index < 0) index = _quotedict.Quotes.Count + index;
 
             var quote = _quotedict.Quotes[index];
-            return $"{quote.Text} @ {_quotedict.AuthorName}";
+            
+            return $"{quote.Text}<br> @ [id{_quotedict.AuthorId}|{_quotedict.AuthorName}]";
         }
 
         public Quote this[int index] => GetQuote(index);
