2482f8fd3215781328dcc495af38614c458ecd88
Dmitry Nevedomsky <dimchik01050@gmail.com>
2018-03-11 18:33:36 +0300

    extension methods and constant

--- bsuir-chat-bot/VkMessageParser.cs
+++ bsuir-chat-bot/VkMessageParser.cs
@@ -7,30 +7,49 @@ namespace bsuir_chat_bot
 {
     public static class VkMessageParser
     {
+        private const int MESSAGE = 4;
+
         public static List<Message> ParseLongPollMessage(string content)
         {
             var responseDict = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(content);
             if (!responseDict.ContainsKey("updates")) return null;
-            List<Message> messageList = new List<Message>();
+            var messageList = new List<Message>();
             foreach (var update in responseDict["updates"])
             {
-                if (update[0] == 4)
+                if (update[0] == MESSAGE)
                 {
-                    Message parsed = new Message();
-                    parsed.Body = update[5];
-                    parsed.Id = update[1];
-                    parsed.Date = DateTimeOffset.FromUnixTimeSeconds((long)update[4]).UtcDateTime;
+                    var parsed = new Message
+                    {
+                        Body = update[5],
+                        Id = update[1],
+                        Date = DateTimeOffset.FromUnixTimeSeconds((long) update[4]).UtcDateTime
+                    };
+
                     if (update[3] > 2000000000)
                     {
-                        parsed.ChatId = (update[3] - 2000000000);
+                        parsed.ChatId = ((long) update[3]).ToChatId();
                         parsed.FromId = update[6]["from"];
                     }
                     else
+                    {
                         parsed.FromId = update[3];
+                    }
+
                     messageList.Add(parsed);
                 }
             }
+
             return messageList;
+        }
+
+        private static long ToPeerId(this long id)
+        {
+            return id + 2000000000;
+        }
+
+        private static long ToChatId(this long id)
+        {
+            return id - 2000000000;
         }
     }
 }
