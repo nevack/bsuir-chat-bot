f64a03b4e4ac036092bfb55d9187a9d5a8f96510
Sateviss <sateviss@gmail.com>
2018-05-31 00:22:21 +0300

    Started StatsProvider

---
+++ bsuir-chat-bot/Providers/StatsProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using VkNet.Model.RequestParams;
+
+namespace bsuir_chat_bot
+{   
+    public class StatsProvider : VkBotProvider
+    {
+        public StatsProvider()
+        {
+            Functions = new Dictionary<string, string>
+            {
+                {"rankwords", "rankwords [timespan] - top ten users in this chat ranked by word count"},
+                {"rankchars", "rankchars [timespan] - top ten users in this chat ranked by character count"},
+                {"rankmessages", "rankmessages [timespan] - top ten users in this chat ranked by message count"}
+            };
+        }
+
+        private IEnumerable<Message> GetHistory(long peerId, DateTime targeDateTime)
+        {
+            List<Message> output;
+            
+        }
+
+        protected override MessagesSendParams _handle(VkNet.Model.Message command)
+        {
+            var (func, _) = command.ParseFunc();
+
+            string message;
+            
+            switch (func.ToLowerInvariant())
+            {
+                case "words":
+                    message = Ping();
+                    break;
+                default:
+                    throw new KeyNotFoundException();
+            }
+
+
+            var param = new MessagesSendParams
+            {
+                Message = message,
+                PeerId = command.GetPeerId()
+            };
+
+            return param;
+        }
+    }
+}
