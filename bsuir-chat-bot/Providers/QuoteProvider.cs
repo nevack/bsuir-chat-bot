1929e77ecb58355e1a41bf9a5f4272414c626426
Sateviss <sateviss@gmail.com>
2018-05-31 21:29:48 +0300

    I'm a fucking retard

--- bsuir-chat-bot/Providers/QuoteProvider.cs
+++ bsuir-chat-bot/Providers/QuoteProvider.cs
@@ -138,7 +138,7 @@ namespace bsuir_chat_bot
                     message = $"Added author [{args[0]}|{args[1]}]";
                     break;
                 case "listauthors":
-                    _quotedict.ToList().ForEach(pair => message+=$"[id{pair.Value.AuthorId}|{pair.Value.AuthorName}] - {pair.Value.Quotes.Count}");
+                    _quotedict.ToList().ForEach(pair => message+=$"[id{pair.Value.AuthorId}|{pair.Value.AuthorName}] - {pair.Value.Quotes.Count}\n");
                     break;
                 default:
                     throw new ArgumentException("No matching command found");
